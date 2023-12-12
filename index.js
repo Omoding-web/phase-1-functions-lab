dfaad0771ade857b559b7bda473acd1f3268241d
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-12 12:40:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function distanceFromHqInBlocks(dist){
-    let a = (Math.abs(dist - 42));
-    return a;
+function distanceFromHqInBlocks(location){
+    let b = (Math.abs(location - 42));
+    return b;
 }
 
-function distanceFromHqInFeet(dist){
-    let a = (Math.abs(dist - 42));
-    return (a * 264);
+function distanceFromHqInFeet(location){
+    let b = (Math.abs(location - 42));
+    return (b * 264);
 
 }
-function distanceTravelledInFeet(dist1, dist2){
-    let a = Math.abs(dist1 - dist2);
-    return(a * 264);
+function distanceTravelledInFeet(location1, location2){
+    let b = Math.abs(location1 - location2);
+    return(b * 264);
 }
 
-function calculatesFarePrice(dist1, dist2){
-    let a = (Math.abs(dist1 - dist2) * 264);
-    if (a <= 400){
+function calculatesFarePrice(location1, location2){
+    let b = (Math.abs(location1 - location2) * 264);
+    if (b <= 400){
         return 0;
-    } else if (a > 400 && a <= 2000){
-        return ((0.02) * (a - 400));
-    } else if (a > 2000 && a <= 2500){
+    } else if (b > 400 && b <= 2000){
+        return ((0.02) * (b - 400));
+    } else if (b > 2000 && b <= 2500){
         return 25;
     } else {
         return("cannot travel that far");
