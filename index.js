14889448fae9725fef1617d26edd628bd7d3cc92
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-12 10:48:37 +0300

    Completed assisgnment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(dist){
+    let a = (Math.abs(dist - 42));
+    return a;
+}
+
+function distanceFromHqInFeet(dist){
+    let a = (Math.abs(dist - 42));
+    return (a * 264);
+
+}
+function distanceTravelledInFeet(dist1, dist2){
+    let a = Math.abs(dist1 - dist2);
+    return(a * 264);
+}
+
+function calculatesFarePrice(dist1, dist2){
+    let a = (Math.abs(dist1 - dist2) * 264);
+    if (a <= 400){
+        return 0;
+    } else if (a > 400 && a <= 2000){
+        return ((0.02) * (a - 400));
+    } else if (a > 2000 && a <= 2500){
+        return 25;
+    } else {
+        return("cannot travel that far");
+    }
+}
